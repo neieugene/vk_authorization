ecbcdb07fd87d690fcfaca3a7ad15d3d4336c9a6
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-12 00:38:34 +0300

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     root to: "devise/sessions#new"
   end
   devise_for :users,
-  controllers: { omniauth_callbacks: "client#index" }
+  controllers: { omniauth_callbacks: "clients#index" }
 end
