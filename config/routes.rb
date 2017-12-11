b51f6ce931e8044f71e94e635681b7125ee10663
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-12 00:07:11 +0300

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :clients
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  devise_for :users#,
-  #controllers: { omniauth_callbacks: "users/vkontakte" }
+  devise_for :users,
+  controllers: { omniauth_callbacks: "client#index" }
 end
