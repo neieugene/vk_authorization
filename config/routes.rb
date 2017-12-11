fa2c2c5da0e0f4665e8a829b90b91e9937bb5ab8
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-11 23:46:31 +0300

    Fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   devise_for :users, # <- внимание, запятая, вызов ещё не кончился
   controllers: { omniauth_callbacks: "users/vkontakte" }
   # У меня контроллер Users::OmniauthCallbacksController
