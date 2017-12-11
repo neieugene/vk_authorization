3d554010e07398b2602e117a2bda933aedfa19fa
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-11 23:57:45 +0300

    fix redirection

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  devise_for :users, # <- внимание, запятая, вызов ещё не кончился
-  controllers: { omniauth_callbacks: "users/vkontakte" }
-  # У меня контроллер Users::OmniauthCallbacksController
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users#,
+  #controllers: { omniauth_callbacks: "users/vkontakte" }
 end
