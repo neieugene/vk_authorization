31fed8c5b08838bf53bcf36646d13cc7cbba53c1
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-13 22:25:45 +0300

    fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
   resources :humen
   devise_for :users,
     controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
