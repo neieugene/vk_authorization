1aff89fc9c6e90c065d803cc00d871a1efc5fbe6
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-13 22:12:29 +0300

    Refactoring

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
-  	@par = request.env["omniauth.auth"]
-  	@extra = request.env["omniauth.auth"].extra
-  	@first_name = request.env["omniauth.auth"].extra.raw_info.last_name
-  	if (Human.where(:vk_id => request.env["omniauth.auth"].extra.raw_info.id).exists?)
-  		human = Human.find(:vk_id => request.env["omniauth.auth"].extra.raw_info.id)
-  	else
+  	unless (Human.where(:vk_id => request.env["omniauth.auth"].extra.raw_info.id).exists?)
   		human = Human.create(:vk_id => request.env["omniauth.auth"].extra.raw_info.id, :first_name => request.env["omniauth.auth"].extra.raw_info.first_name, :last_name => request.env["omniauth.auth"].extra.raw_info.last_name, :photo => request.env["omniauth.auth"].extra.raw_info.photo_100)
   		human.save
   	end
-  	@human = human
   	redirect_to "/humen"
   end
 end
