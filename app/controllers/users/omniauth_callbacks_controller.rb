b0c8772520e0041b8395ff392258129673ffcdfb
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-13 21:09:20 +0300

    Fix controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,8 +7,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   		human = Human.find(:vk_id => request.env["omniauth.auth"].extra.raw_info.id)
   	else
   		human = Human.create(:vk_id => request.env["omniauth.auth"].extra.raw_info.id, :first_name => request.env["omniauth.auth"].extra.raw_info.first_name, :last_name => request.env["omniauth.auth"].extra.raw_info.last_name)
+  		human.save
   	end
   	@human = human
-  	#redirect_to "/humen"
+  	redirect_to "/humen"
   end
 end
