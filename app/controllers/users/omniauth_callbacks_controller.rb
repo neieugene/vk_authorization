27dc04fcbfac2e31ae973a6b451abf96fb86fcd2
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-13 20:40:56 +0300

    fix view

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def vkontakte
   	@par = request.env["omniauth.auth"]
+  	@extra = request.env["omniauth.auth"].extra
+  	@first_name = request.env["omniauth.auth"].extra.raw_info.last_name
   end
 end
