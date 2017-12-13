c1176b7d56d48ae5003267303d79a45a066614e6
neieugene <Nehvedovich.Eugene@yandex.ru>
2017-12-13 20:49:29 +0300

    fix controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,5 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   	@par = request.env["omniauth.auth"]
   	@extra = request.env["omniauth.auth"].extra
   	@first_name = request.env["omniauth.auth"].extra.raw_info.last_name
+  	if (Human.where(:vk_id => request.env["omniauth.auth"].extra.raw_info.id).exists?)
+  		human = Human.find(:vk_id => request.env["omniauth.auth"].extra.raw_info.id)
+  	else
+  		human = Human.create(:vk_id => request.env["omniauth.auth"].extra.raw_info.id, :first_name => request.env["omniauth.auth"].extra.raw_info.first_name, :last_name => request.env["omniauth.auth"].extra.raw_info.last_name)
+  	end
+  	redirect_to "/humen"
   end
 end
---
+++ app/views/humen/_human.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! human, :id, :created_at, :updated_at
+json.url human_url(human, format: :json)
